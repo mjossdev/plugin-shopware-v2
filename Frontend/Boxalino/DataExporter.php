a233cabbaec2554e5a72bb53359e92e2bfef8880
tapun21 <marcel.zenger@boxalino.com>
2017-12-19 06:43:04 +0100

    Exporter changes for vote field.

--- Frontend/Boxalino/DataExporter.php
+++ Frontend/Boxalino/DataExporter.php
@@ -264,8 +264,9 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter {
             }
 
             $data[] = $row;
-            if($count++%2 == 0) {
+            if(sizeof($data) > 1000) {
                 $files->savepartToCsv('product_stream.csv', $data);
+                $data = [];
             }
 
         }
@@ -284,27 +285,61 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter {
         $data = array();
         $header = true;
         $sql = $db->select()
-            ->from(array('a' => 's_articles_vote'), array('vote' => 'points'))
-            ->join(array('a_d' => 's_articles_details'), 'a.articleID = a_d.articleID', array('a_d.id'))
-            ->where('a.active = 1');
+            ->from(array('a' => 's_articles_vote'),
+                array('average' => new Zend_Db_Expr("SUM(a.points) / COUNT(a.id)"), 'articleID'))
+            ->where('a.active = 1')
+            ->group('a.articleID');
         if ($this->delta) {
             $sql->where('a.articleID IN(?)', $this->deltaIds);
         }
-
         $stmt = $db->query($sql);
         while ($row = $stmt->fetch()) {
-            if (!isset($this->shopProductIds[$row['id']])) {
-                continue;
-            }
             if ($header) {
                 $data[] = array_keys($row);
                 $header = false;
             }
+            if(sizeof($data) > 1000) {
+                $files->savepartToCsv('vote.csv', $data);
+                $data = [];
+            }
             $data[] = $row;
+        }
+        $files->savepartToCsv('vote.csv', $data);
+        $referenceKey = $this->bxData->addResourceFile($files->getPath('vote.csv'), 'articleID', ['average']);
+
+        $data = array();
+        $header = true;
+        $sql = $db->select()
+            ->from(array('a' => 's_articles'), array())
+            ->join(
+                array('d' => 's_articles_details'),
+                $this->qi('d.articleID') . ' = ' . $this->qi('a.id') . ' AND ' .
+                $this->qi('d.kind') . ' <> ' . $db->quote(3),
+                array('id', 'articleID')
+            );
+        if ($this->delta) {
+            $sql->where('a.id IN(?)', $this->deltaIds);
+        }
+        $stmt = $db->query($sql);
+        if ($stmt->rowCount()) {
+            while ($row = $stmt->fetch()) {
+                if(!isset($this->shopProductIds[$row['id']])) {
+                    continue;
+                }
+                if ($header) {
+                    $data[] = array_keys($row);
+                    $header = false;
+                }
+                $data[$row['id']] = array('id' => $row['id'], 'articleID' => $row['articleID']);
+                if(sizeof($data) > 1000) {
+                    $files->savepartToCsv('product_vote.csv', $data);
+                    $data = array();
+                }
+            }
         }
         $files->savepartToCsv('product_vote.csv', $data);
         $attributeSourceKey = $this->bxData->addCSVItemFile($files->getPath('product_vote.csv'), 'id');
-        $this->bxData->addSourceStringField($attributeSourceKey, "vote", "vote");
+        $this->bxData->addSourceNumberField($attributeSourceKey, "vote", "articleID", $referenceKey);
     }
 
     /**
@@ -944,6 +979,7 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter {
                 $page++;
             }
         }
+        $files->savepartToCsv('product_translations.csv', $data);
         $doneCases = null;
         $attributeSourceKey = $this->bxData->addCSVItemFile($files->getPath('product_translations.csv'), 'id');
 
