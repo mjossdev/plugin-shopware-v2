7f34107c2defa9f1f9b115c50879fd84a5fb4045
Dana Negrescu <dana@boxalino.com>
2018-08-29 19:29:10 +0200

    M2P-10 added header params for node pinning

--- Frontend/Boxalino/lib/BxClient.php
+++ Frontend/Boxalino/lib/BxClient.php
@@ -239,6 +239,8 @@ class BxClient
 
 	private function getP13n($timeout=2, $useCurlIfAvailable=true){
 
+        list($sessionid, $profileId) = $this->getSessionAndProfile();
+
 		if (isset($this->requestMap['dev_bx_socket'])) {
 			$this->setSocket($this->requestMap['dev_bx_socket']);
 		}
@@ -253,9 +255,9 @@ class BxClient
 		}
 
 		if($useCurlIfAvailable && function_exists('curl_version')) {
-			$transport = new \Thrift\Transport\P13nTCurlClient($this->host, $this->port, $this->uri, $this->schema, $this->curl_timeout);
+			$transport = new \Thrift\Transport\P13nTCurlClient($this->host, $this->port, $profileId, $this->uri, $this->schema, $this->curl_timeout);
 		} else {
-			$transport = new \Thrift\Transport\P13nTHttpClient($this->host, $this->port, $this->uri, $this->schema);
+			$transport = new \Thrift\Transport\P13nTHttpClient($this->host, $this->port, $profileId, $this->uri, $this->schema);
 		}
 
 		$transport->setAuthorization($this->p13n_username, $this->p13n_password);
@@ -343,8 +345,7 @@ class BxClient
 			'User-Host'	  => array($this->getIP()),
 			'User-SessionId' => array($sessionid),
 			'User-Referer'   => array(@$_SERVER['HTTP_REFERER']),
-			'User-URL'	   => array($this->getCurrentURL()),
-            'X-BX-PROFILEID' => array($profileid)
+			'User-URL'	   => array($this->getCurrentURL())
 		);
 	}
 
