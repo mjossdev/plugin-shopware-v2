a1703925db8778c2db3e09cfd52cdec4167042b3
Dana Negrescu <dana@boxalino.com>
2019-03-04 14:45:20 +0100

    update on upstream conditions

--- Frontend/Boxalino/SearchInterceptor.php
+++ Frontend/Boxalino/SearchInterceptor.php
@@ -519,7 +519,7 @@ class Shopware_Plugins_Frontend_Boxalino_SearchInterceptor
         if($streamId) {
             $streamConfig = $this->BxData()->getStreamById($streamId);
             if($streamConfig[$streamId]['conditions']){
-                $conditions = $this->unserialize(json_decode($streamConfig['conditions'], true));
+                $conditions = $this->unserialize(json_decode($streamConfig[$streamId]['conditions'], true));
                 $filter = $this->getConditionFilter($conditions);
                 if(is_null($filter)) {
                     return null;
