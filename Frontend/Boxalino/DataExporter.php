32f8d3feeebebbee181a2a9188cb70455b92832f
Dana Negrescu <dana@boxalino.com>
2019-06-18 15:27:16 +0200

    customer and transaction export updates: adding address details

--- Frontend/Boxalino/DataExporter.php
+++ Frontend/Boxalino/DataExporter.php
@@ -1444,6 +1444,38 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
         return $filteredAttributes;
     }
 
+    /**
+     * Getting a list of transaction address attributes and the table it comes from
+     * To be used in the general SQL select
+     *
+     * @return array
+     */
+    public function getTransactionAddressAttributes()
+    {
+        $addressAttributes = array();
+        $db = $this->db;
+        $db_name = $db->getConfig()['dbname'];
+        $tables = ['s_order_billingaddress', 's_order_shippingaddress'];
+        $select = $db->select()
+            ->from(array('col' => 'information_schema.columns'), array('COLUMN_NAME', 'TABLE_NAME'))
+            ->where('col.TABLE_SCHEMA = ?', $db_name)
+            ->where("col.TABLE_NAME IN (?)", $tables);
+
+        $attributes = $db->fetchAll($select);
+        foreach ($attributes as $attribute) {
+            if($attribute['COLUMN_NAME'] == 'orderID' || $attribute['COLUMN_NAME'] == 'id' || $attribute['COLUMN_NAME'] == 'userID'){
+                continue;
+            }
+
+            $key = "{$attribute['TABLE_NAME']}.{$attribute['COLUMN_NAME']}";
+            $addressAttributes[$key] = "shipping_" . $attribute['COLUMN_NAME'];
+            if($attribute['TABLE_NAME'] == 's_order_billingaddress'){
+                $addressAttributes[$key] = "billing_" . $attribute['COLUMN_NAME'];
+            }
+        }
+
+        return $addressAttributes;
+    }
 
     /**
      * Getting a list of product attributes and the table it comes from
@@ -1506,12 +1538,10 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
         foreach ($this->_config->getAccountLanguages($account) as $shop_id => $language) {
             $data = array();
             $countMax = 1000000;
-            $limit = 5000;
+            $limit = 3000;
             $totalCount = 0;
             $page = 1;
             while ($countMax > $totalCount + $limit) {
-
-                // get all customers
                 $sql = $db->select()
                     ->from(
                         array('s_user'),
@@ -1521,6 +1551,26 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
                         array('s_user_billingaddress'),
                         $this->qi('s_user_billingaddress.userID') . ' = ' . $this->qi('s_user.id'),
                         array()
+                    )
+                    ->joinLeft(
+                        array('s_core_countries'),
+                        $this->qi('s_user_billingaddress.countryID') . ' = ' . $this->qi('s_core_countries.id'),
+                        array("countryiso")
+                    )
+                    ->joinLeft(
+                        array('s_core_countries_states'),
+                        $this->qi('s_user_billingaddress.stateID') . ' = ' . $this->qi('s_core_countries_states.id'),
+                        array("statename" => "name")
+                    )
+                    ->joinLeft(
+                        array('s_core_locales'),
+                        $this->qi('s_user.language') . ' = ' . $this->qi('s_core_locales.id'),
+                        array("languagecode" => "locale")
+                    )
+                    ->joinLeft(
+                        array('s_core_shops'),
+                        $this->qi('s_user.subshopID') . ' = ' . $this->qi('s_core_shops.id'),
+                        array("subshopname" => "name")
                     )
                     ->where($this->qi('s_user.subshopID') . ' = ?', $shop_id)
                     ->limit($limit, ($page - 1) * $limit);
@@ -1562,6 +1612,7 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
 
     /**
      * Transactions export
+     * Exporting detailed billing and shipping address for the items/order
      *
      * @throws Zend_Db_Adapter_Exception
      * @throws Zend_Db_Statement_Exception
@@ -1571,8 +1622,9 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
         $db = $this->db;
         $account = $this->getAccount();
         $files = $this->getFiles();
-        $transaction_attributes = $this->getTransactionAttributes($account);
-        $transaction_properties = array_flip($transaction_attributes);
+        $attributes = $this->getTransactionAttributes($account);
+        $addressAttributes = $this->getTransactionAddressAttributes();
+        $transaction_properties = array_flip(array_merge($attributes, $addressAttributes));
 
         $quoted2 = $db->quote(2);
         $oInvoiceAmount = $this->qi('s_order.invoice_amount');
@@ -1594,7 +1646,7 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
         $header = true;
         $data = array();
         $countMax = 10000000;
-        $limit = 5000;
+        $limit = 3000;
         $totalCount = 0;
         $date = date("Y-m-d H:i:s", strtotime("-1 month"));
         $mode = $this->_config->getTransactionMode($account);
@@ -1606,6 +1658,36 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
                     ->from(
                         array('s_order'),
                         $transaction_properties
+                    )
+                    ->joinLeft(
+                        array('s_order_shippingaddress'),
+                        $this->qi('s_order.id') . ' = ' . $this->qi('s_order_shippingaddress.orderID'),
+                        array()
+                    )
+                    ->joinLeft(
+                        array('s_order_billingaddress'),
+                        $this->qi('s_order.id') . ' = ' . $this->qi('s_order_billingaddress.orderID'),
+                        array()
+                    )
+                    ->joinLeft(
+                        array('c_b' => 's_core_countries'),
+                        $this->qi('s_order_billingaddress.countryID') . ' = ' . $this->qi('c_b.id'),
+                        array("billing_countryiso"=>"countryiso")
+                    )
+                    ->joinLeft(
+                        array('s_b'=>'s_core_countries_states'),
+                        $this->qi('s_order_billingaddress.stateID') . ' = ' . $this->qi('s_b.id'),
+                        array("billing_statename" => "name")
+                    )
+                    ->joinLeft(
+                        array('c_s' => 's_core_countries'),
+                        $this->qi('s_order_shippingaddress.countryID') . ' = ' . $this->qi('c_s.id'),
+                        array("shipping_countryiso"=>"countryiso")
+                    )
+                    ->joinLeft(
+                        array('s_s'=>'s_core_countries_states'),
+                        $this->qi('s_order_shippingaddress.stateID') . ' = ' . $this->qi('s_s.id'),
+                        array("shipping_statename" => "name")
                     )
                     ->joinLeft(
                         array('s_user'),
@@ -1622,9 +1704,35 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
                         $this->qi('a_d.ordernumber') . ' = ' . $this->qi('s_order_details.articleordernumber'),
                         array('articledetailsID' => 'id')
                     )
+                    ->joinLeft(
+                        array('o_s' => 's_core_states'),
+                        $this->qi('s_order.status') . ' = ' . $this->qi('o_s.id'),
+                        array('statusname' => 'name')
+                    )
+                    ->joinLeft(
+                        array('o_s_c' => 's_core_states'),
+                        $this->qi('s_order.cleared') . ' = ' . $this->qi('o_s_c.id'),
+                        array('clearedname' => 'name')
+                    )
+                    ->joinLeft(
+                        array('o_p' => 's_core_paymentmeans'),
+                        $this->qi('s_order.paymentID') . ' = ' . $this->qi('o_p.id'),
+                        array('paymentname' => 'name')
+                    )
+                    ->joinLeft(
+                        array('s_core_locales'),
+                        $this->qi('s_order.language') . ' = ' . $this->qi('s_core_locales.id'),
+                        array("languagecode" => "locale")
+                    )
+                    ->joinLeft(
+                        array('s_core_shops'),
+                        $this->qi('s_order.subshopID') . ' = ' . $this->qi('s_core_shops.id'),
+                        array("shopname" => "name")
+                    )
                     ->where($this->qi('s_order.subshopID') . ' = ?', $shop_id)
                     ->limit($limit, ($page - 1) * $limit)
                     ->order('s_order.ordertime DESC');
+
                 if ($mode == 1) {
                     $sql->where('s_order.ordertime >= ?', $date);
                 }
@@ -1632,7 +1740,7 @@ class Shopware_Plugins_Frontend_Boxalino_DataExporter
 
                 if ($stmt->rowCount()) {
                     while ($row = $stmt->fetch()) {
-                        // @note list price at the time of the order is not stored, only the final price
+                        /** @note list price at the time of the order is not stored, only the final price **/
                         $row['discounted_price'] = $row['price'];
                         $row['guest_id']="";
                         $data[] = $row;
