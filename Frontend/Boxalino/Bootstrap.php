7760225a52cc17a4fda29d58100760207b2bc72b
danneg <dana@boxalino.com>
2021-03-23 14:36:04 +0100

    [3.2.1]

--- Frontend/Boxalino/Bootstrap.php
+++ Frontend/Boxalino/Bootstrap.php
@@ -52,7 +52,7 @@ class Shopware_Plugins_Frontend_Boxalino_Bootstrap
     }
 
     public function getVersion() {
-        return '3.2';
+        return '3.2.1';
     }
 
     public function getInfo() {
