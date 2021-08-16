834c65e4ad38a0ba1fffde054f5077233c62b6bb
danneg <dana@boxalino.com>
2021-08-16 14:31:42 +0200

    [4.0.4] ac fix by @marceljoss

--- Frontend/Boxalino/Bootstrap.php
+++ Frontend/Boxalino/Bootstrap.php
@@ -52,7 +52,7 @@ class Shopware_Plugins_Frontend_Boxalino_Bootstrap
     }
 
     public function getVersion() {
-        return '4.0.3';
+        return '4.0.4';
     }
 
     public function getInfo() {
